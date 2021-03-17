cf703966c2e7e4260426c2bf5f711cae28b2644a
PaulineChane <pauline.chane@gmail.com>
2021-03-17 04:03:46 -0700

    heap sort complexities added

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,8 +2,14 @@
 require_relative "./min_heap.rb"
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity:  O(n log n) - for n elements, each element requires O(log n) worst case to insert into 
+# heap -- remaining operations are O(n) and at a lower degree, highest degree of complexity is 
+# therefore n * log n => O(n log n)
+
+# Space Complexity: O(n) to store n nodes, one node per element, in
+# newly created heap. From the heap, the input list was overwritten
+# to conserve an extra degree of space, though I don't think this 
+# actually changes the space complexity big O... 
 def heap_sort(list)
   heap = MinHeap.new
 
