81faa61d2208568d657da8e5274fc66d944faa30
PaulineChane <pauline.chane@gmail.com>
2021-03-17 03:53:18 -0700

    heapsort passing, all need time/space complexity

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,22 @@
 
+require_relative "./min_heap.rb"
 
 # This method uses a heap to sort an array.
 # Time Complexity:  ?
 # Space Complexity: ?
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  heap = MinHeap.new
+
+  list.each do |int|
+    heap.add(int)
+  end
+
+  i = 0 
+  
+  until i == list.length
+    list[i] = heap.remove
+    i += 1
+  end
+
+  return list;
 end
