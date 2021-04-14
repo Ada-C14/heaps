8c1f2c67e7b5f323381cf9cf95bc4a145f07a63a
Anna Kim <anna.kim93@gmail.com>
2021-04-14 13:12:37 -0700

    add and heap_up methods

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,10 +14,21 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  
+  # Time Complexity: O(log n)
+  # >> O(1) create new HeapNode
+  # >> O(1) add node to end of array
+  # >> O(log n) heap_up / heap_up_helper 
+  
+  # Space Complexity: O(1)
+  # >> O(1) (adding to an existing array) --> sometimes array may need to double, but amortized cost is O(1)
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    new_node = HeapNode.new(key, value)
+    @store << new_node
+
+    heap_up(@store.size - 1)
+
+    return
   end
 
   # This method removes and returns an element from the heap
@@ -58,7 +69,27 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def heap_up(index)
-    
+    raise ArgumentError, 'There is no node at this index' if @store[index].nil?
+    return if index == 0
+
+    if index % 2 == 0
+      heap_up_helper(index, 2)
+    else
+      heap_up_helper(index, 1)
+    end
+
+    return
+  end
+
+  def heap_up_helper(index, offset)
+    parent_index = (index - offset) / 2
+    while @store[index].key < @store[parent_index].key && index > -1 && parent_index > -1
+      swap(index, parent_index)
+      index = parent_index
+      parent_index = (index - offset) / 2
+    end
+
+    return
   end
 
   # This helper method takes an index and 
@@ -71,7 +102,8 @@ class MinHeap
   # If you want a swap method... you're welcome
   def swap(index_1, index_2)
     temp = @store[index_1]
-    @store[index_1] = @store[index_2]
-    @store[index_2] = temp
+    @store[index_1], @store[index_2] = @store[index_2], temp
+    # @store[index_1] = @store[index_2]
+    # @store[index_2] = temp
   end
 end
