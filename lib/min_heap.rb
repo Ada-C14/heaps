ebbe8c9ca1bec700b4bc6e2f3e3d336d3c538500
Tram Bui <pbui17@cmc.edu>
2021-04-22 20:33:34 -0500

    completed the min_heap methods

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -15,7 +15,7 @@ class MinHeap
 
   # This method adds a HeapNode instance to the heap
   # Time Complexity: O(log n)
-  # Space Complexity: ?
+  # Space Complexity: O(log n) - potential stack call when we heap_up
   def add(key, value = key)
     @store << HeapNode.new(key, value)
 
@@ -25,13 +25,15 @@ class MinHeap
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
   # Time Complexity: O(log n)
-  # Space Complexity: ?
+  # Space Complexity: O(log n) - potential head_down stack call
   def remove()
     return nil if @store.empty?
 
+    p @store
     swap(0, @store.length - 1)
     result = @store.pop
 
+    p @store
     # start heap_down with the root(index 0)
     heap_down(0) unless @store.empty?
     result.value
@@ -64,8 +66,8 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
   def heap_up(index)
     # oh so we're using an array implementation 
     # compare the new node with it's parent 
@@ -79,51 +81,43 @@ class MinHeap
       # recursive step
       parent = (index - 1) / 2
 
-      if @store[index].key < @store[parent].key
-        swap(index, parent)
+      if @store[parent].key > @store[index].key
+        swap(parent,index)
         heap_up(parent)
+      else
+        return @store
       end
     end
   end
 
-  # This helper method takes an index and 
-  #  moves it up the heap if it's smaller
-  #  than it's parent node.
+
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
   def heap_down(index)
     # heap_down until you've reached a leaf node?
     # swap smaller to two children if it's a min heap
     # repeat until we stop making swap or reach a leaf node
 
-    # check to see if the node has children 
-
-    # left_child = i * 2 + 1
-    # right_child = i * 2 + 2
-
-    # base case is when the node doesn't have any children
-
-    
-    # if index * 2 + 2  < @store.length - 1
-    #   right_child = index * 2 + 2
-    # elsif index * 2 + 1 < @store.length - 1
-    #   left_child = index * 2 + 1
-    # else
-    #   # it doesn't have any child 
-    #   return @store
-    # end
-
-
     # recursive case is when there is at least one child
-    left_child = index * 2 + 1
-    right_child = index * 2 + 2
-    if @store.length - 1 > left_child && @store.length - 1 > right_child
+    left = index * 2 + 1
+    right = index * 2 + 2
+    # if left child index is less or equal to the length of arr, it's a leaf node
+    if @store.length <= left # && @store.length - 1 > right_child
+      # node doesn't have any children
       return @store
-    else
+    else # recursive case, we're not at a leaf node, heap_down if we're able to 
+      # try left, swap if we're able
+      # if we can't, try right,swap if we're able 
+      # if we can't swap, return 
       if @store[index].key > @store[left].key
         swap(index, left)
-        heap_down(left)
+        # not exactly sure why we're passing down the index again?
+        heap_down(index)
       elsif @store[index].key > @store[right].key
-        swap(index, left)
-        heap_down(right)
+        swap(index, right)
+        heap_down(index)
+      else
+        return @store
       end
     end
   end
