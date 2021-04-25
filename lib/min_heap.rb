016ef7a5b474d5c83e95ed949b28e27b1886b9c1
Genevieve Neely <genevieve.hood@gmail.com>
2021-04-25 12:48:40 -0700

    implement min_heap

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -8,24 +8,29 @@ class HeapNode
 end
 
 class MinHeap
-
   def initialize
     @store = []
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(n)
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    new_node = HeapNode.new(key, value)
+    @store << new_node
+    index = @store.length - 1
+    return heap_up(index)
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(n)
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    swap(0, -1)
+    deleted = @store.pop
+    heap_down(0)
+    return deleted.value
   end
 
 
@@ -44,10 +49,10 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(1)
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.length == 0 ? true : false
   end
 
   private
@@ -55,17 +60,25 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
   def heap_up(index)
-    
+    parent = (index - 1) / 2
+    return if parent < 0 || @store[parent].key <= @store[index].key 
+    swap(parent, index)
+    return heap_up(parent)
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    left = index * 2 + 1
+    right = 2 * (index + 1)
+    min = index
+    swap(right, index) if right <= @store.length && @store[right].key < @store[index].key 
+    swap(left, index) if left <= @store.length && @store[left].key < @store[index].key
+    heap_down(index) if min!= index
   end
 
   # If you want a swap method... you're welcome
