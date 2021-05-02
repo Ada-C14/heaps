1d5a36d47cb8846bb38e44331d0541a644eb0ef7
Christabel Sebastian <christabel.escarez@gmail.com>
2021-05-01 21:18:56 -0700

    heap_sort implemented

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,18 @@
-
+require_relative 'min_heap'
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity:  O(n log n) where n is the number of elements in the given array
+# Space Complexity: O(n)
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  heap = []
+  list.each_with_index do |element, current|
+    heap << element
+    parent = current / 2
+    while heap[current] < heap[parent] do
+      heap[parent], heap[current] = heap[current], heap[parent]
+      current = parent
+      parent = current / 2
+    end
+  end
+  return heap
 end
