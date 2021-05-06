f5252f7ac3cf642c0cc8dc78905f07439c1c368a
Yinjing Lin <lyingjin@amazon.com>
2021-05-05 23:34:26 -0700

    heap_sort not complete

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,13 @@
 
-
 # This method uses a heap to sort an array.
 # Time Complexity:  ?
 # Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+def heapsort(list)
+  return list if list.empty?
+  heap = MinHeap.new
+
+  list.each do |n|
+    heap.add(n)
+  end
+  return heap.store.map {|node| node.key}
 end
