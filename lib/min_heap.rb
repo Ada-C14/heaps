3e23652b0bee04ccb0076b3d91dae483fe85590c
roshni-patel <rpatel01@wesleyan.edu>
2021-05-08 19:35:20 -0500

    Worked on heap down and heap sort

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,8 +14,8 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(log n)
   def add(key, value = key)
     @store << HeapNode.new(key, value)
     heap_up(@store.length - 1)
@@ -23,8 +23,8 @@ class MinHeap
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(log n)
   def remove()
     return nil if @store.empty?
 
@@ -32,7 +32,7 @@ class MinHeap
     result = @store.pop 
 
     heap_down(0) unless @store.empty? 
-    return result 
+    return result.value  
   end
 
 
@@ -51,8 +51,8 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(1)
   def empty?
     return @store.empty?
   end
@@ -62,8 +62,8 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
   def heap_up(index)
     parent = (index - 1) / 2 
     if index == 0 
@@ -77,7 +77,21 @@ class MinHeap
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
   def heap_down(index)
+    left_child_index = (index * 2) + 1 
+    right_child_index = (index * 2) + 2 
+
+    if left_child_index >= @store.length
+      return 
+    elsif @store[right_child_index] && @store[right_child_index].key < @store[left_child_index].key && @store[right_child_index].key < @store[index].key
+      swap(right_child_index, index)
+      heap_down(right_child_index)
+    elsif @store[left_child_index].key < @store[index].key
+      swap(left_child_index, index)
+      heap_down(left_child_index)
+    end
   end
 
   # If you want a swap method... you're welcome
