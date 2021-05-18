84276b640d36fc276f210a80d63e42d07ee708ce
Alice D <alicedilanchian@gmail.com>
2021-05-17 23:41:57 -0500

    heap_down

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -52,8 +52,8 @@ class MinHeap
 
   private
 
-  # This helper method takes an index and
-  #  moves it up the heap, if it is less than it's parent node.
+  # The heap up method takes an index and
+  #  moves it up the heap, if it is less than its parent node.
   #  It could be **very** helpful for the add method.
   # Time complexity: O(log n)
   # Space complexity: O(log n)
@@ -70,10 +70,28 @@ class MinHeap
 
   end
 
-  # This helper method takes an index and 
-  #  moves it up the heap if it's smaller
-  #  than it's parent node.
+  # The heap down method takes an index and 
+  #  moves it DOWN the heap if it's larger
+  #  than its parent node
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
+
   def heap_down(index)
+    return @store if index == 0
+
+    left_child_index = 2 * index + 1
+    right_child_index = 2 * index + 2
+
+    if @store[index].key > left_child_index
+      swap(left_child_index,index)
+      heap_down(left_child_index)
+    elsif @store[index].key > right_child_index
+      swap(left_child_index,index)
+      heap_down(left_child_index)
+    else
+      return @store
+    end
+
   end
 
   # If you want a swap method... you're welcome
