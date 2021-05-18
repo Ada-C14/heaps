7485e63b0cb85955274d991361cd09ab5c0c69a6
Alice D <alicedilanchian@gmail.com>
2021-05-17 22:32:37 -0500

    heap_up

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -17,7 +17,7 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+
   end
 
   # This method removes and returns an element from the heap
@@ -55,17 +55,25 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n)
+  # Space complexity: O(log n)
   def heap_up(index)
+    return @store if index == 0 
     
+    parent_index = (1/2)*(index - 1)
+    return @store if @store[index].key >= @store[parent_index].key
+    
+    if @store[index].key < @store[parent_index].key
+      swap(parent_index,index)
+      heap_up(parent_index)
+    end
+
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
   end
 
   # If you want a swap method... you're welcome
