79869f31227eb2eb943a6d4d37ecbba950c279dc
Alice D <alicedilanchian@gmail.com>
2021-05-20 01:38:43 -0500

    heapsort implemented & heap_down adjusted

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,24 @@
 
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity:  O(n log n)
+# Space Complexity: O(n)
+def heapsort(list)
+  heapsorted_array = []
+  return heapsorted_array if list.empty?
+  
+  heap = MinHeap.new()
+  
+  list.each do |item|
+    heap.add(item)
+  end
+
+	list.length.times do
+		removed = heap.remove()
+		heapsorted_array << removed
+	end
+
+	return heapsorted_array
+
+  
 end
--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -56,6 +56,7 @@ class MinHeap
   # Space complexity: O(1)
   def empty?
     return true if @store.length == 0
+    return false if @store.length > 0
   end
 
   private
@@ -89,20 +90,21 @@ class MinHeap
 
     left_child_index = 2 * index + 1
     right_child_index = 2 * index + 2
-    
-    if (@store[index].key > @store[left_child_index].key) && (left_child_index < @store.length - 1)
+   
+    if (left_child_index < @store.length) && (@store[index].key > @store[left_child_index].key)
       swap(left_child_index, index)
       heap_down(index)
-
     end
-    if (@store[index].key > @store[right_child_index].key) && (right_child_index < @store.length - 1)
+    
+    if (right_child_index < @store.length) && (@store[index].key > @store[right_child_index].key)
       swap(right_child_index, index)
       heap_down(index)
     end
 
   end
 
-  # If you want a swap method... you're welcome
+  # If you want a swap method... you're welcome;
+  # thank you :)
   def swap(index_1, index_2)
     temp = @store[index_1]
     @store[index_1] = @store[index_2]
