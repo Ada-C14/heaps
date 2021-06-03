27b4f7559aca382838cd6c9813ee40ee1446da0c
Blaine <b.atsbeha@gmail.com>
2021-06-02 18:37:08 -0700

    heap sort/time-space/comment clean-up

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,18 @@
 
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity:  O(n)
+# Space Complexity: O(n)
+def heapsort(list)
+  # raise NotImplementedError, "Method not implemented yet..."
+  mamasHeap = MinHeap.new()
+  list.each do |e|
+    mamasHeap.add(e)
+  end
+  # p mamasHeap
+  new_list = []
+  while !mamasHeap.empty?
+    new_list << mamasHeap.remove()
+  end
+  return new_list
 end
