2adf0250e3f6d94322a9c9c92be2f2e11a09cefa
Taylor Tompkins <taylormililani@gmail.com>
2021-07-22 16:37:51 -0700

    tests passing

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,18 +14,24 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
-  def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+  # Time Complexity: O(log n)
+  # Space Complexity: O(1)
+  def add(key, value)
+    node = HeapNode.new(key, value)
+    @store << node
+    heap_up(@store.length-1)
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(1)
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    return nil if @store.empty?
+    swap(0, @store.length-1)
+    removed = @store.pop 
+    heap_down(0)
+    return removed.value
   end
 
 
@@ -44,10 +50,10 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(n)
+  # Space complexity: O(1)
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    @store.empty?
   end
 
   private
@@ -55,17 +61,35 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(n)
+  # Space complexity: O(1)
   def heap_up(index)
-    
+    return if index == 0
+
+    parent = (index - 1) / 2
+
+    if @store[index].key < @store[parent].key
+      swap(index, parent)
+      heap_up(parent)
+    end
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    left = index * 2 + 1
+    right = index * 2 + 2
+
+    if right < @store.length
+      min = @store[left].key < @store[right].key ? left : right
+      if @store[index].key > @store[min].key
+        swap(index, min)
+        heap_down(min)
+      end
+    elsif left < @store.length && @store[index].key > @store[left].key
+      swap(index, left)
+    end
   end
 
   # If you want a swap method... you're welcome
