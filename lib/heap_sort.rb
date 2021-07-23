a0aa3bfb11b24473251d448aab16cdc75fb20591
Taylor Tompkins <taylormililani@gmail.com>
2021-07-22 17:03:15 -0700

    forgot heapsort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -4,5 +4,27 @@
 # Time Complexity:  ?
 # Space Complexity: ?
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  n = list.length
+  a = [nil] + list
+
+  (n/2).downto(1) do |i|
+    heapify(a, i, n)
+  end
+
+  while n > 1
+    a[1], a[n] = a[n], a[1]
+    n -= 1
+    heapify(a, 1, n)
+  end
+  a.drop(1)
+end
+
+def heapify(list, parent, limit)
+  root = list[parent]
+  while (child=2 * parent) <= limit
+    child += 1 if child < limit && list[child] < list[child+1]
+    break if root >= list[child]
+    list[parent], parent = list[child], child
+  end
+  list[parent] = root
 end
