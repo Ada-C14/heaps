15eba3f1cd0aa67aa4dde03d1c6533618c8656b1
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-10-26 09:24:22 -0700

    cleaned code

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -18,17 +18,19 @@ class MinHeap
   # Space Complexity: O(log n)
   def add(key, value = key)
     @store << HeapNode.new(key, value)
-    heap_up(@store.length - 1)
+    heap_up(@store.length - 1) # move the new added node up (swap with parent only)
   end
 
+  #This method removes and returns the smallest keyed value from the heap.
   # This method removes and returns an element from the heap
+  #This method removes and returns the smallest keyed value from the heap.
   #   maintaining the heap structure
   # Time Complexity: O(log n)
   # Space Complexity: O(log n)
-  def remove()
+  def remove() # removing the first
     return nil if @store.empty?
 
-    swap(0, @store.length - 1)
+    swap(0, @store.length - 1) # swap the last element with the root
     removed = @store.pop
 
     heap_down(0) unless @store.empty?
@@ -61,34 +63,40 @@ class MinHeap
 
   private
 
+  # down to up
   # This helper method takes an index and
   #  moves it up the heap, if it is less than its parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
-  def heap_up(index)
+  # Time complexity: O(logn)
+  # Space complexity: O(1)
+  def heap_up(index) # move the current index up to the parent and do the needed changes
     parent = (index - 1 ) / 2
 
     if index == 0
       return
     elsif @store[index].key < @store[parent].key
       swap(parent,index)
-      heap_up(parent)
+      heap_up(parent) # keep pushing the parent up till it's  < @store[index].key
     end
   end
 
+  # up to down
+  # this method takes the parent(root) and 'heap it down' to it's right place
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    left_child = (index * 2) + 1
-    right_child = (index * 2) + 2
+    left_child = (index * 2) + 1 # odd is left child
+    right_child = (index * 2) + 2 # even is right child
 
-    if left_child >= @store.length
+    if left_child >= @store.length # reached the limit
       return
+      # if right child < parent AND
+      # if right child < left
     elsif @store[right_child] && @store[right_child].key < @store[left_child].key && @store[right_child].key < @store[index].key
       swap(right_child, index)
       heap_down(right_child)
+      # if left child < parent
     elsif @store[left_child].key < @store[index].key
       swap(left_child, index)
       heap_down(left_child)
